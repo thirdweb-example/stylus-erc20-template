ef9d1e3874d88513ae976f1db88f04357775f314
Yash <kumaryashcse@gmail.com>
2025-05-29 06:03:19 +0530

    ERC20 template

---
+++ src/erc20.rs
@@ -0,0 +1,195 @@
+//! modified from: https://stylus-by-example.org/applications/erc20
+//! 
+//! this contract is not audited
+
+use alloc::string::String;
+use alloy_primitives::{Address, U256};
+use alloy_sol_types::sol;
+use core::marker::PhantomData;
+use stylus_sdk::{
+    evm,
+    msg,
+    prelude::*,
+};
+
+pub trait ERC20Params {
+    const NAME: &'static str;
+    const SYMBOL: &'static str;
+    const DECIMALS: u8;
+}
+
+sol_storage! {
+    pub struct Erc20<T> {
+        // TODO: Name and symbol
+        mapping(address => uint256) balances;
+        mapping(address => mapping(address => uint256)) allowances;
+        uint256 total_supply;
+        PhantomData<T> phantom;
+    }
+
+    pub struct Ownable {
+        address owner;
+    }
+}
+
+sol! {
+    event Transfer(address indexed from, address indexed to, uint256 value);
+    event Approval(address indexed owner, address indexed spender, uint256 value);
+}
+
+#[public]
+impl Ownable {
+    pub fn owner(&self) -> Result<Address, String> {
+        Ok(self.owner.get())
+    }
+
+    pub fn set_owner(&mut self, new_owner: Address) -> Result<(), String> {
+        self._check_owner()?;
+        self._set_owner(new_owner)?;
+
+        Ok(())
+    }
+}
+
+impl Ownable {
+    pub fn _check_owner(&self) -> Result<(), String> {
+        let msg_sender = self.vm().msg_sender();
+        let owner = self.owner.get();
+
+        if msg_sender != owner {
+            return Err("Not authorized".into());
+        }
+
+        Ok(())
+    }
+
+    pub fn _set_owner(&mut self, new_owner: Address) -> Result<(), String> {
+        if new_owner != Address::ZERO {
+            return Err("Zero address".into());
+        }
+
+        self.owner.set(new_owner);
+        
+        Ok(())
+    }
+}
+
+impl<T: ERC20Params> Erc20<T> {
+    pub fn _transfer(
+        &mut self,
+        from: Address,
+        to: Address,
+        value: U256,
+    ) -> Result<(), String> {
+        let mut sender_balance = self.balances.setter(from);
+        let old_sender_balance = sender_balance.get();
+        if old_sender_balance < value {
+            return Err("sdcsdcsdc".into());
+        }
+        sender_balance.set(old_sender_balance - value);
+
+        let mut to_balance = self.balances.setter(to);
+        let new_to_balance = to_balance.get() + value;
+        to_balance.set(new_to_balance);
+
+        evm::log(Transfer { from, to, value });
+        Ok(())
+    }
+
+    pub fn mint(&mut self, address: Address, value: U256) -> Result<(), String> {
+        let mut balance = self.balances.setter(address);
+        let new_balance = balance.get() + value;
+        balance.set(new_balance);
+
+        self.total_supply.set(self.total_supply.get() + value);
+
+        evm::log(Transfer {
+            from: Address::ZERO,
+            to: address,
+            value,
+        });
+
+        Ok(())
+    }
+
+    pub fn burn(&mut self, address: Address, value: U256) -> Result<(), String> {
+        let mut balance = self.balances.setter(address);
+        let old_balance = balance.get();
+        if old_balance < value {
+            return Err("Insufficient balance".into());
+        }
+        balance.set(old_balance - value);
+
+        self.total_supply.set(self.total_supply.get() - value);
+
+        evm::log(Transfer {
+            from: address,
+            to: Address::ZERO,
+            value,
+        });
+
+        Ok(())
+    }
+}
+
+#[public]
+impl<T: ERC20Params> Erc20<T> {
+    pub fn name(&self) -> String {
+        T::NAME.into()
+    }
+
+    pub fn symbol(&self) -> String {
+        T::SYMBOL.into()
+    }
+
+    pub fn decimals() -> u8 {
+        T::DECIMALS
+    }
+
+    pub fn total_supply(&self) -> U256 {
+        self.total_supply.get()
+    }
+
+    pub fn balance_of(&self, owner: Address) -> U256 {
+        self.balances.get(owner)
+    }
+
+    pub fn transfer(&mut self, to: Address, value: U256) -> Result<bool, String> {
+        self._transfer(msg::sender(), to, value)?;
+        Ok(true)
+    }
+
+    pub fn transfer_from(
+        &mut self,
+        from: Address,
+        to: Address,
+        value: U256,
+    ) -> Result<bool, String> {
+        let mut sender_allowances = self.allowances.setter(from);
+        let mut allowance = sender_allowances.setter(msg::sender());
+        let old_allowance = allowance.get();
+        if old_allowance < value {
+            return Err("Insufficient allowance".into());
+        }
+
+        allowance.set(old_allowance - value);
+
+        self._transfer(from, to, value)?;
+
+        Ok(true)
+    }
+
+    pub fn approve(&mut self, spender: Address, value: U256) -> bool {
+        self.allowances.setter(msg::sender()).insert(spender, value);
+        evm::log(Approval {
+            owner: msg::sender(),
+            spender,
+            value,
+        });
+        true
+    }
+
+    pub fn allowance(&self, owner: Address, spender: Address) -> U256 {
+        self.allowances.getter(owner).get(spender)
+    }
+}
---
+++ src/lib.rs
@@ -0,0 +1,54 @@
+//! modified from: https://stylus-by-example.org/applications/erc20
+//! 
+//! this contract is not audited
+
+#![cfg_attr(not(any(feature = "export-abi", test)), no_main)]
+extern crate alloc;
+
+mod erc20;
+
+use alloy_primitives::{Address, U256};
+use stylus_sdk::{
+    msg,
+    prelude::*
+};
+use crate::erc20::{Erc20, ERC20Params, Ownable};
+
+struct MyStylusERC20Params;
+impl ERC20Params for MyStylusERC20Params {
+    const NAME: &'static str = "MyStylusERC20"; // Your token name here
+    const SYMBOL: &'static str = "STKN"; // Your token symbol here
+    const DECIMALS: u8 = 18;
+}
+
+sol_storage! {
+    #[entrypoint]
+    struct MyStylusERC20 {
+        #[borrow]
+        Erc20<MyStylusERC20Params> erc20;
+        #[borrow]
+        Ownable ownable;
+    }
+}
+
+#[public]
+#[inherit(Erc20<MyStylusERC20Params>, Ownable)]
+impl MyStylusERC20 {
+    #[constructor]
+    pub fn constructor(&mut self, owner: Address) {
+        let _ = self.ownable._set_owner(owner);
+
+        // TODO: initialize name and symbol here
+    }
+
+    pub fn mint_to(&mut self, to: Address, value: U256) -> Result<(), String> {
+        // self.ownable._check_owner()?; // Remove or modify this check as needed
+        self.erc20.mint(to, value)?;
+        Ok(())
+    }
+
+    pub fn burn(&mut self, value: U256) -> Result<(), String> {
+        self.erc20.burn(msg::sender(), value)?;
+        Ok(())
+    }
+}
